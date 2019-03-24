bb0aab38ff999b91891ac45b91f4eb6095d153e1
happyme531 <2302004040@qq.com>
2019-03-24 15:27:48 +0800

    modified:   ESP8266-clock.ino

--- ESP8266-clock.ino
+++ ESP8266-clock.ino
@@ -1,4 +1,3 @@
-#define BLINKER_WIFI
 #if defined(ARDUINO) && ARDUINO >= 100
 #define printByte(args)  write(args);
 #else
@@ -6,36 +5,41 @@
 #endif
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
 
+#define SECRETKEY "09dbfc38de36"
+#define BLINKER_ESP_SMARTCONFIG
+
 //手里的BH1750貌似和某个其它i2c设备不兼容..
 //如果不使用BH1750,采用ADC读数作为光照强度使用
 //#define USE_BH1750
 bool autoBackLight = true;
+
 //以下#include顺序不要随意修改...(尤其是Wire.h)
+#include <Ticker.h>
 #include <EEPROM.h>
 #include <DallasTemperature.h>
 #include <MD_DS3231.h>
 #include <SparkFunHTU21D.h>
 #include <Adafruit_BMP085.h>
 //#include <LiquidCrystal_I2C.h>
-#include <hd44780.h>                       // main hd44780 header
-#include <hd44780ioClass/hd44780_I2Cexp.h> 
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
 #include <Wire.h>
+#define BLINKER_WIFI
+#define BLINKER_ALIGENIE_SENSOR
+#include <Blinker.h>
 #include <ESP8266WiFi.h>
 #include <smartconfig.h>
 #include <EasyNTPClient.h>
 #include <WiFiUdp.h>
-
 #ifdef USE_BH1750
 #include <BH1750FVI.h>
 #endif
+
+
+
 hd44780_I2Cexp lcd;
 
 
-
-
-//#include <Blinker.h>
-
-//#include <BlinkerWidgets.h>
 
 
 
@@ -58,6 +62,7 @@ HTU21D htu21d;
 
 Adafruit_BMP085 bmp180;
 
+Ticker loopWatchDog;
 
 
 const char *ssid     = "bu~zhun~ceng~wang(2.4G)";
@@ -159,7 +164,7 @@ void plotGraph(float data_[2][20], int index) {
   for (i = 0; i <= 19; i++) {
     data[i] = data_[index][i];
   };
- //float data[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+  //float data[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
   float max_ = -100;
   float min_ = 100;
   for (i = 0; i <= 19; i++) {  //找到最大,最小的元素
@@ -186,75 +191,87 @@ void plotGraph(float data_[2][20], int index) {
   //开始绘图
   //2004一共有20列,每条数据高8x3=24格
   lcd.clear();
-  lcd.home();   
-  uint8_t lcdData[20][3];       
+  lcd.home();
+  uint8_t lcdData[20][3];
   for (i = 0; i <= 19; i++) {
     int height = floor(data[i] * 24);
     for (int j = 0; j <= 2; j++) {
-      Serial.println("Height at pos("+String(i)+","+String(j)+")is "+String(height));
-     if (height <= 8 && height >= 1) {
-       lcdData[i][j]=height;
+      Serial.println("Height at pos(" + String(i) + "," + String(j) + ")is " + String(height));
+      if (height <= 8 && height >= 1) {
+        lcdData[i][j] = height;
       };
-      if(height<=0){
-         lcdData[i][j]=0;
+      if (height <= 0) {
+        lcdData[i][j] = 0;
       };
-      if(height>8);
+      if (height > 8);
       {
-        lcdData[i][j]=8;
+        lcdData[i][j] = 8;
         height -= 8;
       };
     };
   };
- for (uint8_t j = 0; j <= 19; j++) {
-    for ( i = 0; i <= 2; i++) {
-      lcd.setCursor(j,i);
-      if(lcdData[j][i]!=0){
-        lcd.printByte(lcdData[j][i]);
-      }else{
+  for (uint8_t i = 0; i <= 19; i++) {
+    for ( uint8_t j = 0; j <= 2; j++) {
+      lcd.setCursor(i, j);
+      if (lcdData[i][j] != 0) {
+        lcd.printByte(lcdData[i][j]);
+      } else {
         lcd.print(" ");
       };
-//      Serial.println("Height at pos("+String(i)+","+String(j)+")is "+String(height));
-      
+      //      Serial.println("Height at pos("+String(i)+","+String(j)+")is "+String(height));
+
     };
   };
-  
-  lcd.setCursor(0,3);
-  lcd.print("In:"+String(max_)+"|"+String(min_)+"="+String((max_-min_)/18));
+
+  lcd.setCursor(0, 3);
+  lcd.print("In:" + String(max_) + "|" + String(min_) + "=" + String((max_ - min_) / 18));
   delay(3000);
 };
 
+
 unsigned int elapsedSec = 55;
 short tend[2];
-void refreshDisplay() {
-  
-  #ifdef USE_BH1750
-  uint16_t light = LightSensor.GetLightIntensity();
-  #else
-  uint16_t light = (3073-analogRead(A0))/50-35;
-  #endif
-  Serial.println(light);
-  float Temp [2] = {htu21d.readTemperature(), ds18b20.getTempCByIndex(0) - 2.55}; //貌似有误差..?
+uint8_t lastUpdateSec = 19;
+short screen = 0;
 
-  if ( elapsedSec == 600) {   //60秒计算一下趋势
-    elapsedSec = 0;
-    short* Tend;
-    //Tend = calculateTend(Temp);
-    memcpy(tend, calculateTend(Temp), sizeof(tend));       //不知道怎么写，反正这个能用
-
-  };
-  if (getKey() == 4) {
-    plotGraph(historyData, 0);
-  };
-  elapsedSec++;
+void refreshScreen0(uint8_t lastUpdateSec, float Temp[]) {
   //最大程度避免刷新闪屏
   String temp_;
-  temp_ = String(rtc.yyyy) + "/" + String(rtc.mm) + "/" + String(rtc.dd);
-  lcd.setCursor(0, 0);
-  lcd.print("                    "); //20个空格
-  lcd.setCursor(0, 0);
-  lcd.print(temp_);
+  if (lastUpdateSec >= 20) {
+    temp_ = String(rtc.yyyy) + "/";
+    if (rtc.mm < 10) {
+      temp_ += "0" + String(rtc.mm)+ "/";      //给月份补足2位
+    } else {
+      temp_ += String(rtc.mm)+"/";
+    };
+     if (rtc.dd < 10) {
+      temp_ += "0" + String(rtc.dd);      //给日期补足2位
+    } else {
+      temp_ += String(rtc.dd);
+    };
+    
+    lcd.setCursor(0, 0);
+    lcd.print("                    "); //20个空格
+    lcd.setCursor(0, 0);
+    lcd.print(temp_);
+  };
+  
+   if (rtc.h < 10) {
+      temp_ = "0" + String(rtc.h)+ ":";      
+    } else {
+      temp_ = String(rtc.h)+":";
+    };
+   if (rtc.m < 10) {
+      temp_ += "0" + String(rtc.m)+ ":";    
+    } else {
+      temp_ += String(rtc.m)+":";
+    };
+     if (rtc.s < 10) {
+      temp_ += "0" + String(rtc.s);     
+    } else {
+      temp_ += String(rtc.s);
+    };
 
-  temp_ = String(rtc.h) + ":" + String(rtc.m) + ":" + String(rtc.s);
   lcd.setCursor(0, 1);
   lcd.print("                    ");
   lcd.setCursor(0, 1);
@@ -267,56 +284,149 @@ void refreshDisplay() {
   };
   lcd.setCursor(9, 1);
   lcd.print(temp_);
-
-  temp_ = "In:" + String(Temp[0]) + "C" + " " + String(htu21d.readHumidity()) + "%";
-  lcd.setCursor(0, 2);
-  lcd.print("                    ");
-  lcd.setCursor(0, 2);
-  lcd.print(temp_);
-  switch (tend[0]) {
-    case -1: {
-        lcd.print(" -");
-        break;
-      }
-    case 1: {
-        lcd.print(" +");
-        break;
-      }
+  if (lastUpdateSec >= 20) {
+    temp_ = "In:" + String(Temp[0]) + "C" + " " + String(htu21d.readHumidity()) + "%";
+    lcd.setCursor(0, 2);
+    lcd.print("                    ");
+    lcd.setCursor(0, 2);
+    lcd.print(temp_);
+    switch (tend[0]) {
+      case -1: {
+          lcd.print(" -");
+          break;
+        }
+      case 1: {
+          lcd.print(" +");
+          break;
+        }
+    };
   };
 
-  lcd.setCursor(0, 3);
-  lcd.print("                    ");
-  lcd.setCursor(0, 3);
   switch (message)
   {
     case 1: //正常显示外部温度
       {
-        lcd.print("out:" + String(Temp[1]) + "C" + " " + String(bmp180.readPressure()) + "Pa");
-        switch (tend[1]) {
-          case -1: {
-              lcd.print(" -");
-              break;
-            }
-          case 1: {
-              lcd.print(" +");
-              break;
-            }
+        if (lastUpdateSec >= 20) {
+          lcd.setCursor(0, 3);
+          lcd.print("                    ");
+          lcd.setCursor(0, 3);
+          lcd.print("out:" + String(Temp[1]) + "C" + " " + String(bmp180.readPressure()) + "Pa");
+          switch (tend[1]) {
+            case -1: {
+                lcd.print(" -");
+                break;
+              }
+            case 1: {
+                lcd.print(" +");
+                break;
+              }
+          };
         };
         break;
       }
     case 2: //连接wifi中
       {
+        lcd.setCursor(0, 3);
+        lcd.print("                    ");
+        lcd.setCursor(0, 3);
         lcd.print("--Wifi connecting--") ;
+        lastUpdateSec = 20;
         break;
       }
     case 3: //时间获取失败
       {
+        lcd.setCursor(0, 3);
+        lcd.print("                    ");
+        lcd.setCursor(0, 3);
         lcd.print("--Time set failed--");
+        delay(1000);
         ESP.restart();
         break;
       };
+    case 4: //时间获取
+      {
+        lcd.setCursor(0, 3);
+        lcd.print("                    ");
+        lcd.setCursor(0, 3);
+        lcd.print("--Receiving time--");
+        autoSetTime();
+        lastUpdateSec = 20;  //立即刷新
+        break;
+      };
 
-  }
+  };
+};
+
+int aliQueryCount = 0;
+
+void refreshScreen1(uint8_t lastUpdateSec) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("                    ");
+  lcd.setCursor(0, 0);
+  lcd.print("Blinker:");
+  lcd.print(Blinker.connected() ? "OK" : "NG");
+  lcd.setCursor(0, 1);
+  lcd.print("TmallCount:");
+  lcd.print(aliQueryCount);
+  lcd.setCursor(0, 2);
+  lcd.print("Mem:");
+  lcd.print(ESP.getFreeHeap());
+  lcd.setCursor(0, 3);
+  lcd.print("Memfrag:");
+  lcd.print(ESP.getHeapFragmentation());
+
+};
+
+BlinkerNumber tempIn("tempin");
+BlinkerNumber tempOut("tempout");
+BlinkerNumber humidity("humidity");
+BlinkerNumber pressure("pressure");
+
+void refreshDisplay() {
+#ifdef USE_BH1750
+  uint16_t light = LightSensor.GetLightIntensity();
+#else
+  uint16_t light = (3073 - analogRead(A0)) / 50 - 35;
+#endif
+  Serial.println(light);
+  float Temp [2] = {htu21d.readTemperature(), ds18b20.getTempCByIndex(0) - 2.55}; //貌似有误差..?
+
+  if ( elapsedSec == 600) {   //60秒计算一下趋势
+    elapsedSec = 0;
+    short* Tend;
+    //Tend = calculateTend(Temp);
+    memcpy(tend, calculateTend(Temp), sizeof(tend));       //不知道怎么写，反正这个能用
+
+  };
+
+
+  switch (getKey()) {
+    case 1:
+      lastUpdateSec = 20;
+      screen = 0;
+      break;
+    case 2:
+      screen = 1;
+      break;
+    case 3:
+      screen = 2;
+      break;
+    case 4:
+      plotGraph(historyData, 0);
+      break;
+    default: break;
+  };
+
+  switch (screen) {
+    case 0: refreshScreen0(lastUpdateSec, Temp); break;
+    case 1: refreshScreen1(lastUpdateSec = 19); break;
+    default: break;
+
+  };
+
+  elapsedSec++;
+
 
   if (light < 7 && autoBackLight) {
     lcd.noBacklight();
@@ -324,12 +434,58 @@ void refreshDisplay() {
     lcd.backlight();
   };
 
+  if (lastUpdateSec >= 20) {
+    lastUpdateSec = 0;
+    tempIn.print(Temp[0]);
+    tempOut.print(Temp[1]);
+    humidity.print(htu21d.readHumidity());
+    pressure.print(bmp180.readPressure());
+  } else {
+    lastUpdateSec++;
+  };
+
+  //更新各个屏幕
+
+
+
+
+};
+
+void aliQuery(int32_t queryCode) {
+  aliQueryCount++;
+  switch (queryCode) {
+    case BLINKER_CMD_QUERY_ALL_NUMBER:
+      BlinkerAliGenie.temp(htu21d.readTemperature());
+      BlinkerAliGenie.humi(htu21d.readHumidity());
+      BlinkerAliGenie.pm25(-1);
+      BlinkerAliGenie.print();
+      break;
+
+    case  BLINKER_CMD_QUERY_TEMP_NUMBER:
+      BlinkerAliGenie.temp(htu21d.readTemperature());
+      BlinkerAliGenie.print();
+      break;
+    case BLINKER_CMD_QUERY_HUMI_NUMBER:
+      BlinkerAliGenie.humi(htu21d.readHumidity());
+      BlinkerAliGenie.print();
+      break;
+
+    //ds18b20.requestTemperatures();
+    //BlinkerAliGenie.pm25((float)(ds18b20.getTempCByIndex(0) - 2.55));
+    case BLINKER_CMD_QUERY_PM25_NUMBER:
+      BlinkerAliGenie.pm25(-1);
+      BlinkerAliGenie.print();
+      break;
+  };
 };
 
 
 void autoSetTime() {
   rtc.control(DS3231_12H, DS3231_ON);
-
+  rtc.control(DS3231_SQW_ENABLE, DS3231_OFF);
+  rtc.control(DS3231_SQW_TYPE, DS3231_SQW_1HZ);
+  rtc.control(DS3231_SQW_ENABLE, DS3231_ON);
+  Serial.println("SQW:" + String(rtc.status(DS3231_SQW_ENABLE)));
   unsigned long Time = ntpClient.getUnixTime();
 
   //暴力把unix时间转换为年月日...
@@ -350,7 +506,7 @@ void autoSetTime() {
     Year = 2000 + Y2KTime / 146097 * 400 + (Y2KTime % 146097 - 1) / 36524 * 100 + ((Y2KTime % 146097 - 1) % 36524 + 1) / 1461 * 4 + (((Y2KTime % 146097 - 1) % 36524 + 1) % 1461 - 1) / 365;
     YTime = (((Y2KTime % 146097 - 1) % 36524 + 1) % 1461 - 1) % 365 + 1;
   }
-  Day = YTime-1;
+  Day = YTime;
   unsigned char f = 1; //循环标志
 
   while (f)
@@ -473,21 +629,7 @@ void autoSetTime() {
     rtc.pm = 0;
   };
   Serial.println(((Time  % 86400L) / 3600));
-  //if ( ((Time % 3600) / 60) < 10 ) {
-  // // In the first 10 minutes of each hour, we'll want a leading '0'
-  //  Serial.print('0');
-  // display.print('0');
-  //}
-
-  // Serial.print((Time  % 3600) / 60); // print the minute (3600 equals secs per minute)
-  //display.print((Time  % 3600) / 60);
   rtc.m = (Time  % 3600) / 60;
-  // Serial.print(':');
-  /// if ( (Time % 60) < 10 ) {
-  //   // In the first 10 seconds of each minute, we'll want a leading '0'
-  //   Serial.print('0');
-  //}
-  //Serial.println(Time % 60); // print the second
   rtc.s = Time % 60;
   rtc.writeTime();
 end_: ;
@@ -518,6 +660,18 @@ unsigned char LY(unsigned int y)//判断是否为闰年
     }
   };
 */
+uint32_t lastLoopTime=0;
+void checkLoop(){
+  if(millis()-lastLoopTime>=5000){       //主循环可能卡死了..
+    digitalWrite(LED_BUILTIN,LOW);
+    refreshDisplay();
+  }else{
+    digitalWrite(LED_BUILTIN,HIGH);
+  };
+  if(millis()-lastLoopTime>=50000){     //卡住500s重启
+    ESP.restart();
+  };
+};
 
 #define key4 D5
 #define key3 D6
@@ -543,30 +697,35 @@ unsigned short getKey() {
 
 void setup() {
   Serial.begin(115200);
-
-  lcd.begin(20,4);
+  BLINKER_DEBUG.stream(Serial);
+  lcd.begin(20, 4);
+  lcd.backlight();
+  //lcd.print("startup..");
   //lcd.init();                      // 初始化...
-  #ifdef USE_BH1750
+#ifdef USE_BH1750
   LightSensor.begin();
-  #endif
+#endif
   Wire.begin();
   ds18b20.begin();
-  lcd.backlight();
+
   htu21d.begin();
   bmp180.begin();
+  
+  pinMode(LED_BUILTIN ,OUTPUT);
+
   pinMode(key1, INPUT);             //界面切换的四个按键，高电平=按下
   pinMode(key2, INPUT);
   pinMode(key3, INPUT);
   pinMode(key4, INPUT);
 
-  uint8_t L8[8] = {0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};    //8段竖条，反正内存够~
-  uint8_t L7[8] = {0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};
-  uint8_t L6[8] = {0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};
-  uint8_t L5[8] = {0x00, 0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};
-  uint8_t L4[8] = {0x00, 0x00, 0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f};
-  uint8_t L3[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x1f, 0x1f, 0x1f};
-  uint8_t L2[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1f, 0x1f};
-  uint8_t L1[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1f};
+  static const uint8_t L8[8] = {0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};    //8段竖条，反正内存够~
+  static const uint8_t L7[8] = {0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};
+  static const uint8_t L6[8] = {0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};
+  static const uint8_t L5[8] = {0x00, 0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f};
+  static const uint8_t L4[8] = {0x00, 0x00, 0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f};
+  static const uint8_t L3[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x1f, 0x1f, 0x1f};
+  static const uint8_t L2[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1f, 0x1f};
+  static const uint8_t L1[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1f};
   lcd.createChar(8, L8);
   lcd.createChar(7, L7);
   lcd.createChar(6, L6);
@@ -575,7 +734,6 @@ void setup() {
   lcd.createChar(3, L3);
   lcd.createChar(2, L2);
   lcd.createChar(1, L1);
-
 
   EEPROM.begin(32); //32byte eeprom,保存esptouch之后的wifi
 
@@ -611,22 +769,34 @@ void setup() {
       };
     };
   };
+
+  loopWatchDog.attach(3,checkLoop);
+  ESP.wdtEnable(500);
 };
 
+
 void loop() {
-  // Blinker.run();
+  ESP.wdtFeed();
+  delay(0);
+  lastLoopTime=millis();
+  
+  if (calibrated == true) {
+    Blinker.run();
+  };
+  
 
   if (millis() - prevms > 960) { //refresh every second
     prevms = millis();
     message = 1;
     rtc.readTime();
-    delay(10);
     if (WiFi.status() != WL_CONNECTED) {
       message = 2;
     };
 
     if (WiFi.status() == WL_CONNECTED && calibrated == false) {
-      autoSetTime();
+      Blinker.begin(SECRETKEY);
+      BlinkerAliGenie.attachQuery(aliQuery);
+      message = 4;
       calibrated = true;
     };
 
